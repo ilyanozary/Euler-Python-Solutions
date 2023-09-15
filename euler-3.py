89216085cf90f1bbff71d3ecf094dbbd30cfadc8
ily <99044801+ilyanozary@users.noreply.github.com>
2023-09-15 23:20:26 +0330

    Add files via upload

---
+++ euler-3.py
@@ -0,0 +1,20 @@
+def prime_factors(number):
+    factors = []
+    divisor = 2
+
+    while divisor <= number:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 1
+
+    return factors
+
+
+number = 600851475143
+factors = prime_factors(number)
+
+print(f"عوامل اولیه عدد {number}:")
+for factor in factors:
+    print(factor)
